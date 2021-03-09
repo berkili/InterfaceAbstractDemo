17ddc1d1e1b30787eb7a3c83bcda94673679878b
Efecan <efecanbayat@gmail.com>
2021-03-10 00:22:06 +0300

    Yazılım Geliştirici Yetiştirme Kampı 5.Hafta 4.Ödev

--- InterfaceAbstractDemo/Program.cs
+++ InterfaceAbstractDemo/Program.cs
@@ -13,6 +13,7 @@ namespace InterfaceAbstractDemo
             BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
             customerManager.Save(new Customer {DateOfBirth = new DateTime(1997,1,1), FirstName = "Ahmet Efecan", LastName = "Bayat", NationalityId = "12345678910" });
 
+
         }
     }
 }
