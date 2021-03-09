d5a1a3f1b6c473138f39d243e140becc7a6f1b80
Efecan <efecanbayat@gmail.com>
2021-03-10 00:20:37 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(1997,1,1), FirstName = "Ahmet Efecan", LastName = "Bayat", NationalityId = "12345678910" });
+
+        }
+    }
+}
